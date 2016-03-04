dc1e81ee002920ea97f929072cb26d0c5423ebff
dandanba <13641194007@139.com>
2016-03-04 12:34:17 +0800

    face＋＋ 测试，失败了，暂时不处理。

---
+++ MePlusClient/app/src/main/java/com/meplus/client/activity/TestFaceActivity.java
@@ -0,0 +1,43 @@
+package com.meplus.client.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.meplus.client.R;
+import com.meplus.client.utils.FIRUtils;
+import com.meplus.client.utils.IntentUtils;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.agora.sample.agora.EntryActivity;
+
+/**
+ * 主页面
+ */
+public class TestFaceActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        FIRUtils.checkForUpdateInFIR(this);
+    }
+
+    @OnClick({R.id.button, R.id.button1, R.id.button2})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                startActivity(IntentUtils.generateIntent(this, TestPNActivity.class));
+                break;
+            case R.id.button1:
+                startActivity(IntentUtils.generateIntent(this, EntryActivity.class));
+                break;
+            case R.id.button2:
+                startActivity(IntentUtils.generateIntent(this, TestScannerActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
+}
