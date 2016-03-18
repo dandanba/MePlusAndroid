a7edbf390acbeb276a3bd8634b8fc1e76ff2ff1a
dandanba <13641194007@139.com>
2016-03-18 18:38:31 +0800

    TestActivity

--- MePlusRobot/app/src/main/java/com/meplus/robot/activity/LogoActivity.java
+++ MePlusRobot/app/src/main/java/com/meplus/robot/activity/LogoActivity.java
@@ -4,21 +4,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
-import com.avos.avoscloud.AVException;
-import com.avos.avoscloud.AVQuery;
-import com.avos.avoscloud.AVRelation;
-import com.avos.avoscloud.FindCallback;
-import com.avos.avoscloud.SaveCallback;
 import com.meplus.client.R;
-import com.meplus.robot.api.model.Robot;
-import com.meplus.robot.api.model.User;
 import com.meplus.robot.utils.IntentUtils;
-import com.meplus.robot.utils.UUIDUtils;
-
-import java.util.List;
 
 import butterknife.ButterKnife;
-import cn.trinea.android.common.util.ToastUtils;
 
 /**
  * 启动Logo页面
@@ -43,45 +32,47 @@ public class LogoActivity extends BaseActivity implements Handler.Callback {
     @Override
     public boolean handleMessage(Message msg) {
         if (msg.what == 1) {
-
-            final String username = "robot2";
-            final String password = "robot2";
-            final String email = "wanggeng@meplusplus.com";
-
-            final Robot robot = new Robot();
-            robot.setRobotName(username);
-            robot.setRobotId(UUIDUtils.getUUID(this));
-
-            final SaveCallback callback = new SaveCallback() {
-                public void done(AVException e) {
-                    if (e == null) {
-                        final User user = User.getCurrentUser(User.class);
-                        if (user == null) {
-                            startActivity(IntentUtils.generateIntent(LogoActivity.this, LoginActivity.class));
-                        } else {
-                            final AVRelation<Robot> relation = user.getRelation(User.RELATION_ROBOTS);
-                            final AVQuery<Robot> query = relation.getQuery();
-                            query.setLimit(1);
-                            query.findInBackground(new FindCallback<Robot>() {
-                                @Override
-                                public void done(List<Robot> results, AVException e) {
-                                    if (e == null) {
-                                        user.setRobotList(results);
-                                    } else {
-                                        ToastUtils.show(LogoActivity.this, e.toString());
-                                    }
-                                    startActivity(IntentUtils.generateIntent(LogoActivity.this, MainActivity.class));
-                                }
-                            });
-                        }
-                        finish();
-                    } else {
-                        ToastUtils.show(LogoActivity.this, e.toString());
-                    }
-                }
-            };
+            startActivity(IntentUtils.generateIntent(LogoActivity.this, TestsActivity.class));
+            finish();
+//
+//            final String username = "robot2";
+//            final String password = "robot2";
+//            final String email = "wanggeng@meplusplus.com";
+//
+//            final Robot robot = new Robot();
+//            robot.setRobotName(username);
+//            robot.setRobotId(UUIDUtils.getUUID(this));
+//
+//            final SaveCallback callback = new SaveCallback() {
+//                public void done(AVException e) {
+//                    if (e == null) {
+//                        final User user = User.getCurrentUser(User.class);
+//                        if (user == null) {
+//                            startActivity(IntentUtils.generateIntent(LogoActivity.this, LoginActivity.class));
+//                        } else {
+//                            final AVRelation<Robot> relation = user.getRelation(User.RELATION_ROBOTS);
+//                            final AVQuery<Robot> query = relation.getQuery();
+//                            query.setLimit(1);
+//                            query.findInBackground(new FindCallback<Robot>() {
+//                                @Override
+//                                public void done(List<Robot> results, AVException e) {
+//                                    if (e == null) {
+//                                        user.setRobotList(results);
+//                                    } else {
+//                                        ToastUtils.show(LogoActivity.this, e.toString());
+//                                    }
+//                                    startActivity(IntentUtils.generateIntent(LogoActivity.this, MainActivity.class));
+//                                }
+//                            });
+//                        }
+//                        finish();
+//                    } else {
+//                        ToastUtils.show(LogoActivity.this, e.toString());
+//                    }
+//                }
+//            };
 //            robot.saveInBackground(callback);
-            callback.done(null);
+//            callback.done(null);
             return true;
         }
         return false;
