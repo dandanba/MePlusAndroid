2577e07eb31a4b6e7801c87662c3876a6ac23c1c
dandanba <13641194007@139.com>
2016-04-08 20:21:47 +0800

    speech

--- MePlusPlus/robot/src/main/java/com/meplus/speech/Constants.java
+++ MePlusPlus/robot/src/main/java/com/meplus/speech/Constants.java
@@ -4,7 +4,10 @@ package com.meplus.speech;
  * Created by dandanba on 4/7/16.
  */
 public class Constants {
+    // lang
     public final static String LANG = "zh_cn";// "en_us"
     // 默认发音人
     public final static String VOICER = "nannan";
+    // app_id
+    public final static String APP_ID = "55d7d8a9";
 }
--- MePlusPlus/robot/src/main/java/com/meplus/speech/SpeechUtil.java
+++ MePlusPlus/robot/src/main/java/com/meplus/speech/SpeechUtil.java
@@ -18,7 +18,7 @@ public class SpeechUtil {
 
         // 注意： appid 必须和下载的SDK保持一致，否则会出现10407错误
 
-        SpeechUtility.createUtility(application, "appid=" + application.getResources().getString(R.string.app_id));
+        SpeechUtility.createUtility(application, "appid=" + Constants.APP_ID);
 
         // 以下语句用于设置日志开关（默认开启），设置成false时关闭语音云SDK日志打印
         // Setting.setShowLog(false);
