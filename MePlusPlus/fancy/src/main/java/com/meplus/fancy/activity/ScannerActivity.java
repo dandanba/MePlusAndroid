1bbf5e4dd2dea9fadf2fc83e4b4ef89dbc7c5670
dandanba <13641194007@139.com>
2016-04-11 15:57:08 +0800

    扫描后自动退出，如果发现错误，继续扫描。

--- MePlusPlus/fancy/src/main/java/com/meplus/fancy/activity/ScannerActivity.java
+++ MePlusPlus/fancy/src/main/java/com/meplus/fancy/activity/ScannerActivity.java
@@ -3,12 +3,21 @@ package com.meplus.fancy.activity;
 import android.os.Bundle;
 
 import com.meplus.fancy.R;
+import com.meplus.fancy.app.FancyApplication;
 import com.meplus.fancy.events.ScannerEvent;
 import com.meplus.fancy.fragments.SimpleScannerFragment;
+import com.meplus.fancy.model.ApiService;
+import com.meplus.fancy.utils.SignUtils;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.TreeMap;
+
+import cn.trinea.android.common.util.ToastUtils;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * 扫描页面
@@ -31,8 +40,34 @@ public class ScannerActivity extends BaseActivity {
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onScannerEvent(ScannerEvent event) {
-        SimpleScannerFragment simpleScannerFragment =
-                (SimpleScannerFragment) findFragmentById(R.id.frame_layout);
-        simpleScannerFragment.resumeScanner();
+        ToastUtils.show(this, event.getContent());
+
+
+        final TreeMap<String, String> args = new TreeMap<>();
+        final String Data = "samplestring1";
+        final String LibraryId = "samplestring2";
+        args.put("Data", Data);
+        args.put("LibraryId", LibraryId);
+        final String sign = SignUtils.sign(args);
+        final String timestamp = args.remove("time");
+
+        final ApiService apiService = FancyApplication.getInstance().getApiService();
+        apiService.getborrowlistbyrobot(args, timestamp, sign)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        response -> {
+                            ToastUtils.show(this, response.toString());
+                            finish();
+                        },
+                        throwable -> {
+                            ToastUtils.show(this, throwable.toString());
+                            SimpleScannerFragment simpleScannerFragment = (SimpleScannerFragment) findFragmentById(R.id.frame_layout);
+                            simpleScannerFragment.resumeScanner();
+                        },
+                        () -> {
+                        }
+                );
+
     }
 }
