444953519d5d68833da8644ed92579f6620153cc
dandanba <13641194007@139.com>
2016-04-27 11:42:27 +0800

    log

--- MePlusPlus/fancy/src/main/java/com/topeet/serialtest/presenters/SerialPresenter.java
+++ MePlusPlus/fancy/src/main/java/com/topeet/serialtest/presenters/SerialPresenter.java
@@ -1,5 +1,7 @@
 package com.topeet.serialtest.presenters;
 
+import android.util.Log;
+
 import com.meplus.fancy.events.ScannerEvent;
 import com.topeet.serialtest.serial;
 
@@ -9,6 +11,8 @@ import org.greenrobot.eventbus.EventBus;
  * Created by dandanba on 4/23/16.
  */
 public class SerialPresenter {
+    private static final String TAG = SerialPresenter.class.getSimpleName();
+
     // 充磁 0x02 0x56 0x52 0x32 0x03 0x37
     private static final int[] MAGNETIZE_BUFFER = new int[]{0x02, 0x56, 0x52, 0x32, 0x03, 0x37};
     // 消磁 0x02 0x56 0x52 0x31 0x03 0x34
@@ -62,6 +66,7 @@ public class SerialPresenter {
             if (RX != null && RX.length > 0) { // 数据有效
                 mBuffer.append(new String(RX, 0, RX.length));
                 final String buffer = mBuffer.toString();
+                Log.i(TAG, buffer);
                 if (buffer.startsWith("{")) { // JSON 格式
                     if (buffer.endsWith("}")) {// JSON 格式结束
                         EventBus.getDefault().post(new ScannerEvent(buffer));
